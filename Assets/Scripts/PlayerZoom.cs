b4d9db4d952e8358832218eeeb53dddbf6a49ed8
Aziz Hamza <azizhamza0@gmail.com>
2022-10-31 14:16:09 -0700

    adjusted zoomed in sensitivity when aiming down sights
    Adjusted mouse look sensitivity when zoomed in by referencing the rigidbodyfirstpersoncontroller script available from the unity standard asset pack.

--- Assets/Scripts/PlayerZoom.cs
+++ Assets/Scripts/PlayerZoom.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerZoom : MonoBehaviour
 {
     [SerializeField] Camera FPScamera;
     [SerializeField] float zoomedInCamera = 12f;
     [SerializeField] float zoomedOutCamera = 60f;
+    [SerializeField] float ZoomedSensitivity = 0.5f;
+    [SerializeField] float UnzoomedSensitivity = 2f;
 
     bool togglePlayerZoom = false;
+    RigidbodyFirstPersonController movementScript;
+
+    void Start()
+    {
+        movementScript = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     void Update()
     {
@@ -18,14 +27,19 @@ public class PlayerZoom : MonoBehaviour
             {
                 togglePlayerZoom = true;
                 FPScamera.fieldOfView = zoomedInCamera;
+                movementScript.mouseLook.XSensitivity = ZoomedSensitivity;
+                movementScript.mouseLook.YSensitivity = ZoomedSensitivity;
             }
 
             else if (togglePlayerZoom == true)
             {
                 togglePlayerZoom = false;
                 FPScamera.fieldOfView = zoomedOutCamera;
+                movementScript.mouseLook.XSensitivity = UnzoomedSensitivity;
+                movementScript.mouseLook.YSensitivity = UnzoomedSensitivity;
             }
         }
+        
     }
 
 }
